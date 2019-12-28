ead692e982476297d03ef95b33521832d3964b8a
ruinernin <ruinernin@mail.com>
2019-12-28 07:26:16 +0000

    First pass at TV UI

---
+++ plugin.video.foxymeta/resources/lib/tv.py
@@ -0,0 +1,55 @@
+from . import metadata
+from .router import router
+
+import xbmcgui
+import xbmcplugin
+
+
+
+@router.route('/tv/trakt/popular')
+def tv_popular(page=1):
+    for show in metadata.trakt_shows(page=page):
+        li = xbmcgui.ListItem(show['title'])
+        xbmcplugin.addDirectoryItem(router.handle,
+                                    router.build_url(
+                                        tv_show,
+                                        tvdbid=show['ids']['tvdb']),
+                                    li,
+                                    True)
+    xbmcplugin.addDirectoryItem(router.handle,
+                                router.build_url(tv_popular,
+                                                 page=int(page)+1),
+                                xbmcgui.ListItem('Next'),
+                                True)
+    xbmcplugin.endOfDirectory(router.handle)
+
+
+@router.route('/tv/tvdb/show')
+def tv_show(tvdbid=None):
+    for season in metadata.tvdb_show(tvdbid)['airedSeasons']:
+        li = xbmcgui.ListItem(season)
+        xbmcplugin.addDirectoryItem(router.handle,
+                                    router.build_url(
+                                        tv_season,
+                                        tvdbid=tvdbid,
+                                        season=season),
+                                    li,
+                                    True)
+    xbmcplugin.endOfDirectory(router.handle)
+
+
+@router.route('/tv/tvdb/season')
+def tv_season(tvdbid=None, season=None):
+    for episode in metadata.tvdb_season(tvdbid, season):
+        li = xbmcgui.ListItem(episode['episodeName'])
+        xbmcplugin.addDirectoryItem(router.handle,
+                                    '',
+                                    li,
+                                    False)
+    xbmcplugin.endOfDirectory(router.handle)
+
+
+@router.route('/app/tv')
+def root():
+    router.gui_dirlist([(tv_popular, 'Popular TV')],
+                       dirs=True)
